813d0782d00b1e739a942b1d50473f95b68741d6
novaeeken <nova_eeken@hotmail.com>
2023-06-22 12:01:44 +0200

    Add solutions to bonus

--- scripts/main.js
+++ scripts/main.js
@@ -76,16 +76,16 @@ console.log(departments);
 // --------------------------- OPDRACHT 1 -------------------------------
 
 // --- 1A ---
-console.log('De afdeling Sales heeft ' + departments.sales.numberOfEmployees + ' medewerkers');
+console.log(`De afdeling Sales heeft ${departments.sales.numberOfEmployees} medewerkers`);
 
 // --- 1B ---
-console.log('Marketing is een leuke afdeling om te werken. ' + departments.marketing.description);
+console.log(`Marketing is een leuke afdeling om te werken. ${departments.marketing.description}`);
 
 // --- 1C ---
-console.log('De afdeling Customer Service heeft ' + departments['customer-service'].numberOfEmployees + ' medewerkers');
+console.log(`De afdeling Customer Service heeft ${departments[`customer-service`].numberOfEmployees} medewerkers`);
 
 // --- 1D ---
-console.log('Sales is een uitdagende afdeling om te werken als Verkoopmanager. ' + departments.sales.jobs[1].description);
+console.log(`Sales is een uitdagende afdeling om te werken als Verkoopmanager. ${departments.sales.jobs[1].description}`);
 
 // --------------------------- OPDRACHT 2 -------------------------------
 
@@ -96,16 +96,20 @@ console.log(departmentChoice);
 // --- 2B & 2C ---
 switch (departmentChoice) {
     case 'marketing':
-        console.log('Je koos ' + departmentChoice + '. ' + departments.marketing.description);
+        console.log(`Je koos ${departmentChoice}. ${departments.marketing.description}`);
+        document.getElementById('department-description').textContent = departments.marketing.description;
         break;
     case 'sales':
-        console.log('Je koos ' + departmentChoice + '. ' + departments.sales.description);
+        console.log(`Je koos ${departmentChoice}. ${departments.sales.description}`);
+        document.getElementById('department-description').textContent = departments.sales.description;
         break;
     case 'customer service':
-        console.log('Je koos ' + departmentChoice + '. ' + departments['customer-service'].description);
+        console.log(`Je koos ${departmentChoice}. ${departments['customer-service'].description}`);
+        document.getElementById('department-description').textContent = departments['customer-service'].description;
         break;
     default:
         console.error('Ongeldige keuze. Probeer het opnieuw door de pagina te verversen.');
+        document.getElementById('error-message').textContent = 'Ongeldige keuze. Probeer het opnieuw door de pagina te verversen.';
 }
 
 // --------------------------- OPDRACHT 3 (uitgecomment, want dit was een tussenstap) -------------------------------
@@ -142,17 +146,14 @@ switch (departmentChoice) {
 // --------------------------- OPDRACHT 4 -------------------------------
 
 // --- 4A ---
-console.log(departmentChoice + ' is een leuke afdeling om te werken. Er werken op dit moment ' + departments[departmentChoice].numberOfEmployees + ' medewerkers.');
+console.log(`${departmentChoice} is een leuke afdeling om te werken. Er werken op dit moment ${departments[departmentChoice].numberOfEmployees}  medewerkers.`);
 
 // --- 4B ---
-const jobChoice = prompt('Je koos '
-    + departmentChoice
-    + '. '
-    + 'Over welke functie wil je meer weten? Voer een getal tussen 0 en 3 in. 0: '
-    + departments[departmentChoice].jobs[0].title + ', 1: '
-    + departments[departmentChoice].jobs[1].title + ', 2: '
-    + departments[departmentChoice].jobs[2].title + ', 3: '
-    + departments[departmentChoice].jobs[3].title
+const jobChoice = prompt(`Je koos ${departmentChoice}. Over welke functie wil je meer weten? Voer een getal tussen 0 en 3 in: 
+0: ${departments[departmentChoice].jobs[0].title}, 
+1: ${departments[departmentChoice].jobs[1].title}, 
+2: ${departments[departmentChoice].jobs[2].title}, 
+3: ${departments[departmentChoice].jobs[3].title}`
 );
 
 console.log(jobChoice);
@@ -160,17 +161,42 @@ console.log(jobChoice);
 // --- 4C ---
 switch (jobChoice) {
     case '0':
-        console.log('Je koos ' + departments[departmentChoice].jobs[0].title + '. Een uitdagende rol! ' + departments[departmentChoice].jobs[0].description);
+        console.log(`Je koos ${departments[departmentChoice].jobs[0].title}. Een uitdagende rol! ${departments[departmentChoice].jobs[0].description}`);
+        document.getElementById('role-title').textContent = departments[departmentChoice].jobs[0].title;
+        document.getElementById('role-description').textContent = departments[departmentChoice].jobs[0].description;
         break;
     case '1':
-        console.log('Je koos ' + departments[departmentChoice].jobs[1].title + '. Een uitdagende rol! ' + departments[departmentChoice].jobs[1].description);
+        console.log(`Je koos ${departments[departmentChoice].jobs[1].title}. Een uitdagende rol! ${departments[departmentChoice].jobs[1].description}`);
+        document.getElementById('role-title').textContent = departments[departmentChoice].jobs[1].title;
+        document.getElementById('role-description').textContent = departments[departmentChoice].jobs[1].description;
         break;
     case '2':
-        console.log('Je koos ' + departments[departmentChoice].jobs[2].title + '. Een uitdagende rol! ' + departments[departmentChoice].jobs[2].description);
+        console.log(`Je koos ${departments[departmentChoice].jobs[2].title}. Een uitdagende rol! ${departments[departmentChoice].jobs[2].description}`);
+        document.getElementById('role-title').textContent = departments[departmentChoice].jobs[2].title;
+        document.getElementById('role-description').textContent = departments[departmentChoice].jobs[2].description;
         break;
     case '3':
-        console.log('Je koos ' + departments[departmentChoice].jobs[3].title + '. Een uitdagende rol! ' + departments[departmentChoice].jobs[3].description);
+        console.log(`Je koos ${departments[departmentChoice].jobs[3].title}. Een uitdagende rol! ${departments[departmentChoice].jobs[3].description}`);
+        document.getElementById('role-title').textContent = departments[departmentChoice].jobs[3].title;
+        document.getElementById('role-description').textContent = departments[departmentChoice].jobs[3].description;
         break;
     default:
         console.error('Ongeldige keuze. Probeer het opnieuw door de pagina te verversen.');
+        document.getElementById('error-message').textContent = 'Ongeldige keuze. Probeer het opnieuw door de pagina te verversen.';
 }
+
+// --------------------------- OPDRACHT 5 -------------------------------
+
+// --- 5A ---
+// Hiermee vullen we de text-property van het h2-element met id 'role-title'
+// document.getElementById('role-title').textContent = 'Marieke';
+// Hiermee vullen we de text-property van het p-element met id 'department-description'
+// document.getElementById('department-description').textContent = 'Spruitjes...'
+
+// --- 5B ---
+// 1. document-statements toegevoegd in beide switch-statements
+// 2. Onderstaande statement toegevoegd aan de default-cases in beide switch-statements
+// document.getElementById('error-message').textContent = 'Ongeldige keuze. Probeer het opnieuw door de pagina te verversen.';
+
+// --- 5C ---
+// Verwerkt in gehele opdracht
